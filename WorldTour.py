e6b48d5ff92dc0c6c7c53a0461e757d0265a0c33
Aparajita suman <sumankumar86028@gmail.com>
2022-04-27 08:04:01 +0530

    some optimized solution added in this file.

--- WorldTour.py
+++ WorldTour.py
@@ -29,7 +29,7 @@ class Solution:
                 ind = (k + j)
                 if ind > len(petrol) - 1:
                     ind -= len(petrol)
-                print(k, "INDEX -------------> ", ind)
+                # print(k, "INDEX -------------> ", ind)
                 s += (l[ind][0] - l[ind][1])
                 if s < 0:
                     break
@@ -37,9 +37,46 @@ class Solution:
                 return k
         return "Not possible"
 
+    # This is the most optimized methode for solving circular Tour problem
+
+    def WorldTour2(self, petrol, cost, n):
+        dificit = 0
+        balance = 0
+        start = 0
+
+        for i in range(n):
+            balance += (petrol[i]-cost[i])
+
+            if balance<0:
+                dificit += balance
+                balance = 0
+                start = i+1
+
+        if (balance-dificit)>=0:
+            return start
+        else:
+            return -1
+
+    def WorldTour3(self, petrol, cost):
+        for i in range(len(petrol)):
+            s = 0
+            for k in range(len(petrol)):
+                s += (petrol[k]-cost[k])
+                if s<0:
+                    break
+            if s>=0:
+                return i
+
+            petrol.append(petrol[0])
+            petrol.remove(petrol[0])
+            cost.append(cost[0])
+            cost.remove(cost[0])
+
 
 s = Solution()
 petrol = [4, 6, 7, 4]
 cost = [6, 5, 3, 5]
 print(s.WorldTour(petrol, cost))
 print(s.WorldTour1(petrol, cost))
+print(s.WorldTour2(petrol, cost, 4))
+print(s.WorldTour3(petrol, cost))
